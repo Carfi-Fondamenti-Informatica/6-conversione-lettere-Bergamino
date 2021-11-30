70baded0e33900aa0c9c6fb9f380bd6bb3c22953
Bergamino <91837242+Bergamino@users.noreply.github.com>
2021-11-30 14:50:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
 
+char uno(char b){
+    bool r;
+    char d;
+    if((65<=b && b<=90)||(97<=b && b<=122)){
+       r=true;
+        if((65<=b) && (b<=90)){
+            d=b+32;
+        }else if((97<=b)&&(b<=122)){
+            d=b-32;
+        }
+    }else{
+        r=false;
+        d=1;
+    }
+    return d;
+}
